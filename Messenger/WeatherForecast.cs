1c8398f22618c7a22731fb4acb85cd0bfefb25d6
StephanNazarkin <stepan.nazarkin@ukr.net>
2022-04-02 21:05:40 +0300

    The project has been switched to .net 5

--- Messenger/WeatherForecast.cs
+++ Messenger/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace WebApplication1
+using System;
+
+namespace Messenger
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace WebApplication1
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
