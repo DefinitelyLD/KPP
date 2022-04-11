3a0f306c35cc390b7de0211daa5b9bef635bc261
StephanNazarkin <nazarkinstephan@gmail.com>
2022-04-11 16:00:27 +0300

    Empty model, manager and controller for messages have been created

---
+++ Messenger/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.WEB.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
