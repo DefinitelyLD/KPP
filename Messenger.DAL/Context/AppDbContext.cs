9e33ca666d8cc748b7292037e83b1cf693b769a3
vladimir-riabenko <76427828+vladimir-riabenko@users.noreply.github.com>
2022-04-12 14:08:39 +0300

    23 console log remove

--- Messenger.DAL/Context/AppDbContext.cs
+++ Messenger.DAL/Context/AppDbContext.cs
@@ -22,11 +22,6 @@ namespace Messenger.DAL.Context
             Database.EnsureCreated();
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.LogTo(Console.WriteLine);
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
