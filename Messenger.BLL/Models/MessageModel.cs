ee168be55062f587c294e860575e16f9245b3330
StephanNazarkin <nazarkinstephan@gmail.com>
2022-04-12 15:11:46 +0300

    Create dummy methods for MessageController and MessageManager

--- Messenger.BLL/Models/MessageModel.cs
+++ Messenger.BLL/Models/MessageModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Messenger.BLL.Models
 {
-    internal class MessageModel
+    public class MessageModel
     {
+        public int Id { get; set; }
+        public string ChatId { get; set; }
+        public UserModel User { get; set; }
+        public string Text { get; set; }
+        public string ImageId { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     }
 }
--- Messenger/Controllers/MessageController.cs
+++ Messenger/Controllers/MessageController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Messenger.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Messenger.BLL.Managers;
+using Messenger.BLL.Models;
 
 namespace Messenger.WEB.Controllers
 {
     public class MessageController : Controller
     {
-        public IActionResult Index()
+        private readonly MessageManager _messageManager;
+        public void SendMessage(Message msg, int chatId)
         {
-            return View();
+            _messageManager.ManagerSendMessage(msg, chatId);
+        }
+
+        public void EditMessage(Message msg, int chatId)
+        {
+            _messageManager.ManagerEditMessage(msg, chatId);
+        }
+
+        public void DeleteMessage(Message msg, int chatId)
+        {
+            _messageManager.ManagerDeleteMessage(msg, chatId);
+        }
+
+        public void GetMessage(Message msg, int chatId)
+        {
+            _messageManager.ManagerGetMessage(msg, chatId);
         }
     }
 }
