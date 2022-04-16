37adf2175ce437535bb9759f0b81205f3e5f5be7
exxcelsior <andrej.suslov.33@gmail.com>
2022-04-16 19:27:28 +0300

    Controller created

---
+++ Messenger/Controllers/ChatroomController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.WEB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatroomController : Controller
+    {
+        private readonly IChatroomManager _chatroomManager;
+
+        public ChatroomController(IChatroomManager chatroomManager)
+        {
+            _chatroomManager = chatroomManager;
+        }
+
+        public ActionResult<ChatModel> CreateChatroom(ChatModel chat)
+        {
+            return _chatroomManager.CreateChatroom(chat);
+        }
+    }
+}
