07b145c09e6ff48340eb7dd8a3ac64d178ff5b13
StephanNazarkin <nazarkinstephan@gmail.com>
2022-07-21 17:13:12 +0300

    Migrations added

--- Messenger.DAL/Migrations/20220721140833_AdminsRoom.Designer.cs
+++ Messenger.DAL/Migrations/20220721140833_AdminsRoom.Designer.cs
@@ -4,14 +4,16 @@ using Messenger.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Messenger.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220721140833_AdminsRoom")]
+    partial class AdminsRoom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
